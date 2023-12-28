3cd53378b0e9f8ed60d62ad4431723bad2ca3ed5
Nguyen Khanh <nguyenkhanh28031999@gmail.com>
2023-12-28 22:18:33 +0700

    do post creation

---
+++ src/components/auth/register.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+import { Register } from "../antdesigns/auth";
+
+const LoginPage = () => {
+  return (
+    <div className="auth-page">
+      <Register />
+    </div>
+  );
+};
+export default LoginPage;
---
+++ src/pages/footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Footer = () => {
+  return (
+    <div>
+      <div className="logo">LOGO</div>
+      <button type="primary">Logout</button>
+    </div>
+  );
+};
+
+export default Footer;
